c3fe4f312e9b563874ad65aad8b7d4d79ff7a163
kaylatillman <kaylatillman319@gmail.com>
2019-07-10 19:26:55 -0700

    getEleByClass addition

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+    var results = [];
 
+    var searchDom = function (element) {
+        if (element.classList && element.classList.contains(className)) {
+            results.push(element);
+        }  
+        for (var i = 0; i < element.childNodes.length; i++) {
+            searchDom(element.childNodes[i]);
+        }
+    }
+    searchDom(document.body);
+    return results;
 };
